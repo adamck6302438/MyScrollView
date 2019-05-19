cc85d28077fcc1bb5d161ed238f726e74ef3ee02
Ray Kang <adamck6302438@gmail.com>
2019-05-19 11:02:34 -0400

    first commit

---
+++ MyScrollView/MyScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.m
+//  MyScrollView
+//
+//  Created by Frances ZiyiFan on 5/13/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import "MyScrollView.h"
+
+@implementation MyScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
